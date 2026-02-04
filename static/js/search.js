a17197af38e4fed1e776cf98d50ce8c80108dbe8
imspince <spincekvbc@gmail.com>
2026-02-04 09:52:30 +0530

    Create search.js

---
+++ static/js/search.js
@@ -0,0 +1,25 @@
+let posts = []
+let fuse
+
+fetch('/index.json')
+  .then(res => res.json())
+  .then(data => {
+    posts = data
+    fuse = new Fuse(posts, {
+      keys: ['title', 'content'],
+      threshold: 0.35
+    })
+  })
+
+document.addEventListener('input', e => {
+  if (e.target.id !== 'searchBox') return
+
+  const query = e.target.value
+  const results = query ? fuse.search(query).map(r => r.item) : []
+
+  const list = results.map(post =>
+    `<li><a href="${post.url}">${post.title}</a></li>`
+  ).join('')
+
+  document.getElementById('searchResults').innerHTML = list
+})
